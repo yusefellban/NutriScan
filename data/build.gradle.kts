97a1b684b92f75be30d38c39c3fd7426267bcd7a
soutAhmedTayseer <122810273+soutAhmedTayseer@users.noreply.github.com>
2026-07-13 21:30:09 +0300

    🔧setup initial network layer

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -35,4 +35,10 @@ dependencies {
     implementation(project(":domain"))
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.kotlinx.serialization)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.kotlinx.serialization.json)
 }
