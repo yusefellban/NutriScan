f2af007f5f59a37f915fea24633984d1ed695285
Noureldeen Osama Mohamed <138819436+Noureldeen75@users.noreply.github.com>
2026-07-24 18:25:54 +0300

    feat: add exercises domain layer and plan

--- presentation/src/main/kotlin/iti/grad/nutriscan/presentation/exercises/model/ExerciseCategory.kt
+++
@@ -1,8 +0,0 @@
-package iti.grad.nutriscan.presentation.exercises.model
-
-import androidx.annotation.StringRes
-
-data class ExerciseCategory(
-    val id: String,
-    @StringRes val labelRes: Int
-)
