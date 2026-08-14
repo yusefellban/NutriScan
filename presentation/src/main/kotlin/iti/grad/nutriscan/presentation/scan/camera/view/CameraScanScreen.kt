dad92239dd3c18103036c58ae699b2aac047b184
Ashraf Sherif <shifoashraf75@gmail.com>
2026-08-14 14:36:45 +0300

    add missing import for androidx.compose.runtime.key

--- presentation/src/main/kotlin/iti/grad/nutriscan/presentation/scan/camera/view/CameraScanScreen.kt
+++ presentation/src/main/kotlin/iti/grad/nutriscan/presentation/scan/camera/view/CameraScanScreen.kt
@@ -37,6 +37,7 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
